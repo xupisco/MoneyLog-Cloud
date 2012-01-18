3244e39ee168f3fb7bd3d1647ecf47e8d1314c8a
Leandro Voltolino <xupisco@gmail.com>
2012-01-18 19:23:54 -0200

    using OFICIAL dropbox version

--- static/js/moneylog_dropbox.js
+++ static/js/moneylog_dropbox.js
@@ -1,10 +1,6 @@
 var firstRun = true;
 
-function startDropboxInterface() {
-    oneFile = true;
-    useLocalStorage = false;
-    isOnline = true;
-
+function initDropbox() {
     hideUI();
     resetData();
     readFromDropBox();
@@ -13,23 +9,24 @@ function startDropboxInterface() {
 function hideUI() {
     $("#tagsArea").hide();
     $("#charts").hide();
-    $("#report").html('<p style=\'font-size: 18px; text-align: left; line-height: 20px;\'><strong>Carregando arquivo do Dropbox...</strong><br />Local: <Dropbox home>/Apps/MoneyLog Box/moneylog_data.txt</p>');
+    $("#report").html('<p style=\'font-size: 18px; text-align: left; line-height: 20px;\'> ' + i18n.msgLoading.replace('%s', '<Dropbox home>/Apps/MoneyLog Box/moneylog_data.txt')   + '</p>');
 }
 
 function readFromDropBox() {
     $.get('/', { reloading: true }, function(data) {
         $("#editordata").val(data);
         $("#editoropen").css('display', 'inline');
+        
+        resetData();
         rawData = data;
 
         parseData();
         showReport();
-        setupUI();
     });
 }
 
-function updateDropboxFile() {
-    $("#editor").prepend("<div id='saving' style='right: 130px; position: absolute; color: #F44; line-height: 24px;'>Salvando...</div>");
+function saveDropboxData() {
+    $("#editor").prepend("<div id='saving' style='right: 130px; position: absolute; color: #F44; line-height: 24px;'>" + i18n.msgSaving + "</div>");
     $.post('/update', { data: $("#editordata").val() }, function(data) {
         $("#saving").hide();
         //readFromDropBox();
@@ -38,11 +35,4 @@ function updateDropboxFile() {
         showReport();
         editorOff();
     });
-}
-
-function setupUI() {
-    $("#editoropen").click(editorOn);
-    $("#editorclose").click(editorOff);
-    $("#editorsave").click(updateDropboxFile);
-    $("#editordata").bind((isOpera) ? 'onkeypress' : 'onkeydown', insertTab);
 }
